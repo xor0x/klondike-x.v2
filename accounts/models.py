f518ba51afe2a3e4ff6a2e55e5bc31558a2e8f0c
Home <alex.misyuk1@gmail.com>
2018-10-04 08:56:37 +0300

    Fix accounts

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,17 @@ from django.dispatch import receiver
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/', default = 'users/no_avatar.jpg')
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user.username)
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
