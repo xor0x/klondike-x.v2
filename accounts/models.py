160da7dcf2ea7b26846d3a69ccb0254f69c24c9a
xor0x <alex.misyuk1@gmail.com>
2018-10-04 04:42:23 +0000

    Fix

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
