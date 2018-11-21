aea967d13e261256d572c76a2ef8ff018c7a3538
Home <alex.misyuk1@gmail.com>
2018-11-21 19:03:54 +0200

    Fix

--- blog/views.py
+++ blog/views.py
@@ -25,7 +25,7 @@ def post_list(request, tag_slug=None):
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
         object_list = object_list.filter(tags__in=[tag])
-    paginator = Paginator(object_list, 3)
+    paginator = Paginator(object_list, 6)
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
